3acd075cef8843a2954d0ae6de3e4b0c7f8a5a82
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-01-24 13:07:05 +0900

    afterEach() / cleanup()
    afterEach(): test caseのit()の実行が完了する度に呼ばれる関数
    cleanup(): render()でmountしたReact Componentをunmountする

--- src/RenderInput.test.js
+++ src/RenderInput.test.js
@@ -3,10 +3,18 @@ import { render, screen, cleanup } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import RenderInput from './RenderInput'
 
+afterEach(() => cleanup())
+
 describe("Rendering", () => {
     it("Should render all the elements collectly", () => {
         render(<RenderInput />)
         expect(screen.getByRole("button")).toBeTruthy()
         expect(screen.getByPlaceholderText("Enter")).toBeTruthy()
+    })
+})
+
+describe("Input form onChange event", () => {
+    it("Should update input value correctly", () => {
+        
     })
 })
