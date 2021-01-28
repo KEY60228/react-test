2da3139cf3b7df97be12eb8c73a5cc00b6498981
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-01-28 23:40:51 +0900

    expect(hoge).toEqual(fuga)
    hogeとhugaが同じ値であることを確認する

--- src/FrameworkList.test.js
+++ src/FrameworkList.test.js
@@ -9,4 +9,16 @@ describe("Rendering the list with props", () => {
         render(<FrameworkList />)
         expect(screen.getByText("No Data!")).toBeInTheDocument()
     })
+    it("Should render list item correctly", () => {
+        const dummyData = [
+            {id: 1, item: "React dummy"},
+            {id: 2, item: "Angular dummy"},
+            {id: 3, item: "Vue dummy"},
+        ]
+        render(<FrameworkList frameworks={dummyData} />)
+        const frameworkItems = screen.getAllByRole("listitem").map(el => el.textContent)
+        const dummyItems = dummyData.map(el => el.item)
+        expect(frameworkItems).toEqual(dummyItems)
+        expect(screen.queryByText("No Data!")).toBeNull()
+    })
 })
