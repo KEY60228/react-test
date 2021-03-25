fafc003b9fc7abb2010348319450892047d68a03
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-25 13:13:32 +0900

    act()
    RenderHookResultの関数を実行する
    c.f.) https://react-hooks-testing-library.com/reference/api#act

--- src/useCounter.test.js
+++ src/useCounter.test.js
@@ -5,8 +5,30 @@ import { act, renderHook } from '@testing-library/react-hooks'
 afterEach(() => cleanup())
 
 describe("useCounter custom Hook", () => {
-    it("Shouldincrement by 1", () => {
+    it("Should increment by 1", () => {
         const { result } = renderHook(() => useCounter(3))
         expect(result.current.count).toBe(3)
+        act(() => result.current.increment())
+        expect(result.current.count).toBe(4)
+    })
+    it("Should decrement by 1", () => {
+        const { result } = renderHook(() => useCounter(3))
+        act(() => result.current.decrement())
+        expect(result.current.count).toBe(2)
+    })
+    it("Should double the counter value", () => {
+        const { result } = renderHook(() => useCounter(3))
+        act(() => result.current.double())
+        expect(result.current.count).toBe(6)
+    })
+    it("Should triple the counter value", () => {
+        const { result } = renderHook(() => useCounter(3))
+        act(() => result.current.triple())
+        expect(result.current.count).toBe(9)
+    })
+    it("Should reset to zero", () => {
+        const { result } = renderHook(() => useCounter(3))
+        act(() => result.current.reset())
+        expect(result.current.count).toBe(0)
     })
 })
